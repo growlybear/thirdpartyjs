9922082803500553b20dc7fdc54b5e029e86c4bc
Michael Allan <michaelallan@optushome.com.au>
2013-01-03 21:07:51 +1100

    add script loading capabilities to the widget

--- widget/widget.js
+++ widget/widget.js
@@ -1,6 +1,30 @@
 var Stork = (function (window, undefined) {
     var Stork = {};
 
+    function loadScript(url, callback) {
+        var entry,
+            script = document.createElement('script');
+
+        script.async = true;
+        script.src = url;
+
+        entry = document.getElementsByTagName('script')[0];
+        entry.parentNode.insertBefore(script, entry);
+
+        script.onload = script.onreadystatechange = function () {
+            var rdyState = script.readyState;
+
+            if (!rdyState || /complete|loaded/.test(script.readyState)) {
+                callback();
+
+                // detach event handlers to prevent memory leaks in IE
+                // http://msdn.microsoft.com/en-us/library/bb250448(v=vs.85).aspx
+                script.onload = null;
+                script.onreadystatechange = null;
+            }
+        };
+    }
+
     function loadSupportingFiles(callback) {
         // body...
     }
